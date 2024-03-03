7410732b347a43841ab3eecadcc3269523ab12a0
Nelson Loop <nelsonploop@gmail.com>
2024-03-03 13:36:12 -0500

    Fixing env errors.

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 
 from fastapi import FastAPI
 from decouple import config
+import os
 import requests
 
 app = FastAPI()
 
 @app.get("/search/{search_query}")
 async def search(search_query: str):
-    API_KEY = config('RAWG_API_KEY')
+    API_KEY = os.environ.get('RAWG_API_KEY')
     search_url = "https://api.rawg.io/api/games?key={API_KEY}&page_results=5&page=1&search={searchQuery}&search_precise=true"
     search_results = requests.get(search_url).json()
     return search_results
