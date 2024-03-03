1f6be131f0e45e018d751b020c7c9ad7a064ff5a
Nelson Loop <nelsonploop@gmail.com>
2024-03-03 13:27:40 -0500

    Created search API.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # main.py
 
 from fastapi import FastAPI
+from decouple import config
+import requests
 
 app = FastAPI()
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/search/{search_query}")
+async def search(search_query: str):
+    API_KEY = config('RAWG_API_KEY')
+    search_url = "https://api.rawg.io/api/games?key={API_KEY}&page_results=5&page=1&search={searchQuery}&search_precise=true"
+    search_results = requests.get(search_url).json()
+    return search_results
