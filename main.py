aede6e0a3b2b7d5e4d5e4315fb3d0f1010ec5a8d
Nelson Loop <nelsonploop@gmail.com>
2024-03-03 13:43:28 -0500

    resolving errors with env vars.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 @app.get("/search/{search_query}")
 async def search(search_query: str):
-    API_KEY = os.environ.get('RAWG_API_KEY')
-    search_url = "https://api.rawg.io/api/games?key={API_KEY}&page_results=5&page=1&search={searchQuery}&search_precise=true"
+    API_KEY = config('RAWG_API_KEY')
+    search_url = f"https://api.rawg.io/api/games?key={API_KEY}&page_results=5&page=1&search={search_query}&search_precise=true"
     search_results = requests.get(search_url).json()
     return search_results
